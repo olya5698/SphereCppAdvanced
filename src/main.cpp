39be7dd2ebbbc6e9b12cbd7e6f8746cdf9973c4f
Olga Chmereva <o.s.ch@yandex.ru>
2020-10-05 23:48:50 +0300

    Path to process.hpp changed, some style changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include "../include/process.hpp"
+#include "process.hpp"
 
 #include <iostream>
 #include <cstring>
@@ -7,21 +7,21 @@ using namespace std;
 using namespace hw1_process;
 
 int main() {
-        string path = "/usr/bin/cat";
-        string str = "Hello world!";
-        string buf(32,'\0');
-        
-        Process process1(path);
-        process1.writeExact(str.c_str(), str.size());
-        process1.closeStdin();
-        process1.readExact(buf.data(), str.size());
-        process1.close();
+    string path = "/usr/bin/cat";
+    string str = "Hello world!";
+    string buf(32,'\0');
+    
+    Process process1(path);
+    process1.writeExact(str.c_str(), str.size());
+    process1.closeStdin();
+    process1.readExact(buf.data(), str.size());
+    process1.close();
     std::cout << buf << endl;
 
-        path = "/usr/bin/date";
-        Process process2(path);
-        process2.read(buf.data(), buf.size());
-        process2.close();
+    path = "/usr/bin/date";
+    Process process2(path);
+    process2.read(buf.data(), buf.size());
+    process2.close();
     std::cout << buf << endl;
 
     return 0;
